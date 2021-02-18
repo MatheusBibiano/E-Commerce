6060a0a85e772f99d135f9f2b3e4fcbf003b130d
Matheus Bibiano Alves <66566761+MatheusBibiano@users.noreply.github.com>
2021-02-18 13:30:20 -0300

    final commit

--- src/view/home/footer.php
+++ src/view/home/footer.php
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-auto">
-                <span class="copyright">Desenvolvido por <Strong>Matheus</Strong> e <Strong>Gustavo</Strong> &copy;2021</span>
+                <span class="copyright">Desenvolvido por <Strong>Matheus Bibiano</Strong> &copy;2021</span>
             </div>
         </div>
     </div>
